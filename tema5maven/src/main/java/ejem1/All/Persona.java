9ebed7d21a8ed0919ae395547bdd19764313a37c
hmontes <hmontes.dev@gmail.com>
2026-01-19 10:10:02 +0100

    Refactor code structure for improved readability and maintainability

---
+++ tema5maven/src/main/java/ejem1/All/Persona.java
@@ -0,0 +1,53 @@
+package ejem1.All;
+
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Persona {
+    private int id;
+    private String nombre;
+    private boolean casado;
+    private String sexo;
+
+    public Persona() {
+    }
+
+    public Persona(int id, String nombre, boolean casado, String sexo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.casado = casado;
+        this.sexo = sexo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setCasado(boolean casado) {
+        this.casado = casado;
+    }
+
+    public boolean isCasado() {
+        return casado;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+}
