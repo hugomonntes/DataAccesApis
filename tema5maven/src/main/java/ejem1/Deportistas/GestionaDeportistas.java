b24a8a19ff03fb13fb9b33684526668f4e9d2bde
hmontes <hmontes.dev@gmail.com>
2026-01-20 10:44:48 +0100

    feat(gestionDeportistas): add fns

--- tema5maven/src/main/java/ejem1/Deportistas/GestionaDeportistas.java
+++ tema5maven/src/main/java/ejem1/Deportistas/GestionaDeportistas.java
@@ -3,16 +3,19 @@ package ejem1.Deportistas;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.GenericEntity;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/deportista")
 public class GestionaDeportistas {
@@ -52,12 +55,56 @@ public class GestionaDeportistas {
     }
 
     // 3. Buscar jugador (/{id}): devuelve la información relativa al deportista id.
-    // @Path("/{id}")
-    // @GET
-    // public Response infoDeportista(@PathParam("id") int id){
-    //     try (Connection connection = DriverManager.getConnection(url,user,password)){
-            
-    //     } catch (Exception e) {
-    //     }
-    // }
+    @Path("/{id}")
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    public Response infoDeportista(@PathParam("id") int id){
+        try (Connection connection = DriverManager.getConnection(url,user,password)){
+            Statement stm = connection.createStatement();
+            String query = String.format("SELECT * FROM deportistas WHERE id = %d", id);
+            ResultSet rs = stm.executeQuery(query);
+            Deportista deportista = new Deportista(rs.getInt("id"), rs.getString("nombre"), rs.getBoolean("activo"), rs.getString("genero"), rs.getString("deporte"));
+            return Response.ok(deportista).build();
+        } catch (SQLException e) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("No hay deportistas con ese id").build();
+        }
+    }
+
+    // 4. Por deporte (/deporte/{nombreDeporte}): Lista los deportistas de un deporte.
+    @Path("/deporte/{nombreDeporte}")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response buscarPorDeporte(@PathParam("nombreDeporte") String nombreDeporte){
+        ArrayList<Deportista> deportistas = new ArrayList<>();
+        try(Connection connection = DriverManager.getConnection(url, user, password)){
+            Statement stm = connection.createStatement();
+            String query = String.format("SELECT * FROM deportistas WHERE deporte = '%s'",nombreDeporte);
+            ResultSet rs = stm.executeQuery(query);
+            while (rs.next()) {
+                deportistas.add(new Deportista(rs.getInt("id"), rs.getString("nombre"), rs.getBoolean("activo"), rs.getString("genero"), rs.getString("deporte")));
+            }
+            return Response.ok(deportistas).build();
+        } catch(SQLException sqle){
+            return Response.status(Response.Status.BAD_REQUEST).entity("No se encuentra ese deporte").build();
+        }
+    }
+
+    // 5. Activos (/activos): Lista los deportistas activos.
+    @Path("/activos")
+    @GET
+    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    public Response buscarActivos(){
+        ArrayList<Deportista> activos = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(url, user, password)){
+            Statement stm = connection.createStatement();
+            String query = "SELECT * FROM deportistas WHERE activo = true";
+            ResultSet rs = stm.executeQuery(query);
+            while (rs.next()) {
+                activos.add(new Deportista(rs.getInt("id"), rs.getString("nombre"), rs.getBoolean("Activo"), rs.getString("Genero"), rs.getString("Deporte")));
+            }
+            return Response.ok(activos).build();
+        } catch (Exception e) {
+            return Response.status(Status.BAD_REQUEST).entity("No hay deportistas activos").build();
+        }
+    }
 }
